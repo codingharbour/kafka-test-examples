8641a68e6ac839c4acb0f4f97d8ab93a574a5dfb
chromy96 <chromy96@gmail.com>
2023-02-19 18:08:30 +0100

    Add mock producer example

---
+++ mock-producer/src/main/java/com/codingharbour/mockproducer/SomeService.java
@@ -0,0 +1,25 @@
+package com.codingharbour.mockproducer;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class SomeService {
+    private final Producer<String, String> kafkaProducer;
+    private final String topic;
+
+    public SomeService(Producer<String, String> kafkaProducer, String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    /**
+     * Method send a message of (key, key+valuePart) to a kafka topic
+     * @param key a message key
+     * @param valuePart a value part to be transformed to message payload
+     */
+    public void writeDataToKafka(String key, String valuePart){
+        String messageValue = key + valuePart;
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, messageValue);
+        kafkaProducer.send(record);
+    }
+}
---
+++ mock-producer/src/test/java/com/codingharbour/mockproducer/SomeServiceTest.java
@@ -0,0 +1,24 @@
+package com.codingharbour.mockproducer;
+
+import org.apache.kafka.clients.producer.MockProducer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SomeServiceTest {
+
+    private static final String TOPIC = "topic1";
+    MockProducer<String, String> mockProducer = new MockProducer<>(true, new StringSerializer(), new StringSerializer());
+    SomeService someService = new SomeService(mockProducer, TOPIC);
+
+    @Test
+    public void testWriteDataToKafka(){
+        String key="key1";
+        String valuePart = "value1";
+        String expectedValue = key+valuePart;
+        someService.writeDataToKafka(key, valuePart);
+        assertThat(mockProducer.history()).hasSize(1);
+        assertThat(mockProducer.history().get(0).value()).isEqualTo(expectedValue);
+    }
+}
